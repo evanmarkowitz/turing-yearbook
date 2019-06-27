5a61757ab98c05b4933c6ab6649ac4e1bb6a54b9
Evan Markowitz <emarkowitz1@gmail.com>
2019-06-27 14:15:57 -0600

    Wrote cohort test

--- src/components/Cohort.test.js
+++ src/components/Cohort.test.js
@@ -3,9 +3,13 @@ import { shallow, mount } from 'enzyme';
 import Cohort from './Cohort.js';
 
 describe('Person', () => { 
+  let testData = []
+  beforeEach(() => {
+    testData = [{id: 2, name: 'Amy', quote: 'I believe in you, cuties!', superlative: 'Most Likely to Be an Actual Superhero', photo: './amy.jpg'},]
+  })
   it('should match the snapshot with all data passed in correctly', () => { 
     const wrapper = mount( <Cohort
-      
+      data= {testData}
       />);
     expect(wrapper).toMatchSnapshot(); 
   }); 
